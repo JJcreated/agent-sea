ba6395424b4c6ffa38075684085693026f5863af
Gabi Fabiyi <GabiFabiyi@Gabis-MacBook.local>
2024-03-18 15:53:54 +0000

    created frontpage and routes

--- src/components/page_components/Home/Home.tsx
+++ src/components/page_components/Home/Home.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import homeImg from '../../../../public/assets/agent1.jpg';
 import './Home.css';
 
 function Home() {
+
+  const [enterActive, setEnterActive] = useState(false);
+
+  const EnterInactive = () => {
+
+  const handleClick = () => {
+    setEnterActive(true);
+  }
+
   return (
-            <div className='home-page'>
-              <img src={homeImg} className='home-img'/>
+    <div className='home-page'>
+              <img src={homeImg} className='home-image'/>
+              <button className='home-enter-button' onClick={handleClick}>Enter</button>
             </div>
   )
 }
+
+const EnterActive = () => {
+
+  return (
+    <div>Entered</div>
+  )
+
+}
+
+  
+            if(enterActive){
+              return (<EnterActive/>)
+            }
+            else{
+              return (<EnterInactive/>)
+            }
+}
+
+
 
 export default Home
